0102567e3b28ed8a449d54b4cd0c66645efd473a
realmark1r8h <yinhuan4077@163.com>
2022-08-08 14:16:12 +0800

    更新代码

---
+++ doc/docs/part/basic-basic快速开始.md
@@ -0,0 +1,12 @@
+# 快速开始
+
+  A quick start.
+  
+*****
+  编辑manifest.json设置程序的启动方式。 manifest不是必须的，如果不存在，那么程序将使用默认配置，如果存在，那么mainfest.json所在目录必须与程序所在目录相同。manifest支持AppSettings和BrowserWindowSettings两个类 ，详细情况可参照app和BrowserWindow两个模块。如果不使用NodeJs,那么需要指定一个URL作为程序启动时的URL，否则程序将。.
+  
+* **函数参数**  无
+
+* **返回值**
+    
+
---
+++ libNCUI/docs/QuickStart.hpp
@@ -0,0 +1,37 @@
+// Created by amoylel on 17/07/2018.
+// Copyright (c) 2018 amoylel All rights reserved.
+
+#ifndef LIBNCUI_QUICKSTART_51E7465A_7BD9_422B_BADB_9E3D479E949E_HPP__
+#define LIBNCUI_QUICKSTART_51E7465A_7BD9_422B_BADB_9E3D479E949E_HPP__
+
+
+namespace amo {
+
+    /**
+     * @class	快速开始
+     * @desc 快速开始
+     *
+     * @chapter basic
+     *
+     * @brief	A quick start.
+     */
+    
+    class QuickStart {
+    
+        /**
+         * @fn	void QuickStart::配置文件();
+         *
+         * @brief	编辑manifest.json设置程序的启动方式。 manifest不是必须的，如果不存在，那么程序将使用默认配置，如果存在，那么mainfest.json所在目录必须与程序所在目录相同。
+        	manifest支持AppSettings和BrowserWindowSettings两个类 ，详细情况可参照app和BrowserWindow两个模块。
+        	如果不使用NodeJs,那么需要指定一个URL作为程序启动时的URL，否则程序将。.
+         */
+        
+        void foo() {
+        
+        }
+    };
+}
+
+
+
+#endif //LIBNCUI_QUICKSTART_51E7465A_7BD9_422B_BADB_9E3D479E949E_HPP__
