654e6a039ed4c2546a3aaa662c33620314937876
realmark1r8h <yinhuan4077@163.com>
2022-08-15 23:54:12 +0800

    更新代码

--- doc/docs/js/doc.js
+++ doc/docs/js/doc.js
@@ -12780,10 +12780,10 @@ require('./polyfill-done.js');
 
     var chapters = {
          basic       : {col: 1},
+ settings       : {col: 1},
  api       : {col: 1},
  cef       : {col: 1},
  extend       : {col: 1},
- settings       : {col: 1},
 
     };
     var isTouchScreen = 'ontouchstart' in document.documentElement;
